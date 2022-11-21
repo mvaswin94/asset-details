c3513c69baafbac6e003f0b970ea7ac9b9986b2d
mvaswin94 <mvaswin94@gmail.com>
2022-11-21 17:51:48 +0530

    Commented Unwanted Dependencies in pom.xml

--- src/main/java/com/asset/master/MasterController.java
+++ src/main/java/com/asset/master/MasterController.java
@@ -1,5 +1,6 @@
 package com.asset.master;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -32,13 +34,19 @@ public class MasterController {
 	MasterViewRepository masterViewRepo;
 	
 	@GetMapping(value = "entry")
-	public String masterEntry(Model model) {
-		model.addAttribute("masterObj", new MasterView());
+	public String masterEntry(Model model, Master master, MasterDetail masterDetail, MasterParameters masterParameters) {
+		//model.addAttribute("masterObj", new MasterView());
+		model.addAttribute("masterList", new Master());
+		
+		model.addAttribute("masterParametersList",  new MasterParameters());
+		//model.addAttribute("masterParametersList",  masterParameters.getMasterParameters());
+		model.addAttribute("masterDetailList",  new MasterDetail());
 		return "asset-master-entry";
 	}
 	
 	@PostMapping(value = "/entry/submit")
-	public String masterEntrySubmit(@ModelAttribute Master master, MasterDetail masterDetail, MasterParameters masterParameters) {
+	public String masterEntrySubmit(@ModelAttribute Master master, MasterDetail masterDetail, 
+			MasterParameters masterParameters, @RequestBody ArrayList<MasterDetail> masterDetailList) {
 
 		Master obj = new Master();
 		obj.setMasterId(master.getMasterId());
@@ -48,18 +56,29 @@ public class MasterController {
 		MasterDetail obj2 = new MasterDetail();
 		obj2.setMasterId(master.getMasterId());
 		obj2.setMasterName(master.getMasterName());
-		//obj2.setMasterCharacteristic(masterDetail.getMasterCharacteristic());
-		//obj2.setMasterQuestion(masterDetail.getMasterQuestion());
 		obj2.setMasterQuestionTamil(masterDetail.getMasterQuestionTamil());
 		obj2.setMasterQuestionEnglish(masterDetail.getMasterQuestionEnglish());
 		obj2.setMasterQuestionType(masterDetail.getMasterQuestionType());
 		masterDetailRepo.saveAndFlush(obj2);
 		
-//		MasterParameters obj3 = new MasterParameters();
-//		obj3.setMasterId(master.getMasterId());
-//		obj3.setMasterName(master.getMasterName());
-//		obj3.setMasterParameters(masterParameters.getMasterParameters());
-//		masterParametersRepo.saveAndFlush(obj3);
+//		for (int i = 0; i < masterDetailList.size(); i++) {
+//			MasterDetail objLoop = masterDetailList.get(i);
+//
+//			MasterDetail obj4 = new MasterDetail();
+//			obj4.setMasterId(master.getMasterId());
+//			obj4.setMasterName(master.getMasterName());
+//			obj4.setMasterQuestionTamil(objLoop.getMasterQuestionTamil());
+//			obj4.setMasterQuestionEnglish(objLoop.getMasterQuestionEnglish());
+//			obj4.setMasterQuestionType(objLoop.getMasterQuestionType());
+//			masterDetailRepo.saveAndFlush(obj4);
+//
+//		}
+		
+		MasterParameters obj3 = new MasterParameters();
+		obj3.setMasterId(master.getMasterId());
+		obj3.setMasterName(master.getMasterName());
+		obj3.setMasterParameters(masterParameters.getMasterParameters());
+		masterParametersRepo.saveAndFlush(obj3);
 
 		return "redirect:/asset/master/entry";
 	}
@@ -67,13 +86,15 @@ public class MasterController {
 	@GetMapping(value = "/list")
 	public String masterList(Model model) {
 		
-		System.out.println("OBJ TEST2");
+		/* System.out.println("OBJ TEST2"); */
 		/* List<MasterView> obj = masterVieRepo.findAllMasterDetail(); */
 		List<Master> obj = masterRepo.findAll(); 
-		System.out.println("OBJ TEST"+obj);
+		/* System.out.println("OBJ TEST"+obj); */
 		model.addAttribute("page", obj);
 		
 		model.addAttribute("masterSearchObj", new MasterView());
+		
+		model.addAttribute("masterList", obj);
 		
 		return "asset-master-list";
 	}
